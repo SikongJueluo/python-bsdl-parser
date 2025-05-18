9cb6860abe46fc2274079757801f5b7c8ce2b1da
SikongJueluo <selfconfusion@gmail.com>
2025-05-18 14:19:27 +0800

    fix: seperate did not parse well

--- src/BsdlParser.py
+++ src/BsdlParser.py
@@ -19,8 +19,6 @@
 
 
 import os
-from functools import singledispatch
-from typing import Any
 
 from lark import Lark, Token, Tree
 
@@ -28,18 +26,7 @@ from lark import Lark, Token, Tree
 EBNF_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "bsdl.lark")
 
 
-@singledispatch
-def node_to_dict(token: Token, isSeperate=True) -> dict[str,Any]:
-    if isSeperate:
-        return dict(
-            type=token.type,
-            value=str(token.value),
-        )
-    else:
-        return {token.type: str(token.value)}
-
-@node_to_dict.register
-def _(type: str, value, isSeperate: bool = True):
+def node_to_dict(type: str, value, isSeperate: bool = True):
     if isinstance(value, list):
         if isSeperate:
             return dict(
@@ -50,11 +37,22 @@ def _(type: str, value, isSeperate: bool = True):
             unWrappedDict = {}
             for item in value:
                 if isinstance(item, dict) and len(item) == 1:
-                    unWrappedDict[list(item.keys())[0]] = list( item.values() )[0]
+                    unWrappedDict[list(item.keys())[0]] = list(item.values())[0]
                 else:
                     break
             if len(unWrappedDict) == len(value):
                 return unWrappedDict
+            else:
+                return {type: value}
+    elif isinstance(value, dict):
+        if isSeperate:
+            return dict(
+                type=type,
+                children=value,
+            )
+        else:
+            if len(value) == 1 and type == list(value.keys())[0]:
+                return value
             else:
                 return {type: value}
     else:
@@ -75,11 +73,10 @@ class BsdlParser:
         return Lark(ebnfContent)
 
     @staticmethod
-    def ast_process(node:Tree, _depth = 0):
+    def ast_process(node: Tree, _depth=0):
         if _depth == 0 and isinstance(node, Tree) and len(node.children) == 1:
             # 处理根节点
             node = node.children[0]
-
 
     @staticmethod
     def ast_to_dict(node, isSeperate=False, _depth=0):
@@ -91,37 +88,54 @@ class BsdlParser:
         if node is None:
             return
         elif isinstance(node, Token):
-            return node_to_dict(node, isSeperate)
+            # 处理正则表达式名称
+            if node.type[:7] == "__ANON_":
+                return str(node.value)
+            # 处理字符串
+            elif node.type == "ESCAPED_STRING":
+                return node.value
+            # 处理同名字符串
+            elif node.type.lower() == str(node.value).lower():
+                return str(node.value)
+            return node_to_dict(node.type, node.value, isSeperate)
         elif isinstance(node, Tree):
-            if len(node.children) == 1:
-                child = node.children[0]
-                # 处理同名字符串
-                if (
-                    isinstance(child, Token)
-                    and child.type.lower() == str(child.value).lower()
-                ):
-                    return node_to_dict(node.data, child.value, isSeperate)
-            else:
-                # 处理正则表达式名称
-                isAllRegexp = True
-                regexDict = {}
-                for child in node.children:
-                    if not isinstance(child, Token) or not (
-                        child.type[:7] == "__ANON_"
-                    ):
-                        isAllRegexp = False
-                        break
-                    regexDict[child.type] = child.value
-                if isAllRegexp:
-                    return node_to_dict(node.data, regexDict, isSeperate)
+            childrenDict = {}
+            childrenList = []
+            isList = False
+            for child in node.children:
+                childNode = BsdlParser.ast_to_dict(child, isSeperate, _depth)
+                # 处理Null节点
+                if childNode is None:
+                    continue
+                elif isinstance(childNode, str):
+                    isList = True
+                    
+                if not isList:
+                    if isinstance(child, Token) and childrenDict.get(child.type) is None:
+                        childrenDict.update(childNode)
+                    elif isinstance(child, Tree) and childrenDict.get(child.data) is None:
+                        childrenDict.update(childNode)
+                    else:
+                        isList = True
+                        
+                if isList:
+                    if isinstance(child, Token):
+                        childrenList.append(childNode)
+                    else:
+                        childrenList.append(node_to_dict(child.data, childNode, isSeperate))
 
-                # 处理其余情况
-                children = []
-                for child in node.children:
-                    childNode = BsdlParser.ast_to_dict(child, isSeperate, _depth)
-                    if childNode is not None:
-                        children.append(childNode)
-                return node_to_dict(node.data, children, isSeperate)
+                
+            if isList:
+                if len(childrenDict) != 0:
+                    for key, value in childrenDict.items():
+                        childrenList.append(node_to_dict(key, value, isSeperate))
+                # 处理单数情况
+                if len(childrenList) == 1:
+                    return node_to_dict(node.data, childrenList[0], isSeperate)
+                else:
+                    return node_to_dict(node.data, childrenList, isSeperate)
+            else:
+                return node_to_dict(node.data, childrenDict, isSeperate)
 
     @staticmethod
     def find_type(node, type_name):
